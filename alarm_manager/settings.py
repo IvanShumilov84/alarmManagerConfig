fc92d915508d234c27305aa81cc8ee80ad25d76b
Ivan Shumilov <ivan.shumilov84@yandex.ru>
2025-07-10 22:49:31 +0300

    Changes for build app

--- alarm_manager/settings.py
+++ alarm_manager/settings.py
@@ -12,8 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+import sys
+
+if getattr(sys, 'frozen', False):
+    BASE_DIR = Path(sys.executable).resolve().parent
+else:
+    BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
